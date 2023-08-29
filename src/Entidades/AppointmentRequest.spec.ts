ed363e45b3791ae0d947853b68ea721cc82338cb
Picorovisck <ploglamador@hotmail.com>
2023-08-29 18:34:29 -0300

    update

--- src/Entidades/AppointmentRequest.spec.ts
+++ src/Entidades/AppointmentRequest.spec.ts
@@ -37,22 +37,50 @@ test(" Shouldn't be able create an appointment with End less than start ", () =>
 })
 
 
-test('should get the difference between the start and End', () => {
-    const start = new Date()
-    const end = new Date()
+test('should get the difference between the start and End', async () => {
+        
+        const start = new Date()
+        const end = new Date()
 
-    start.setDate(start.getDate() + 1)
-    end.setDate(end.getDate() + 3)
+        start.setDate(start.getDate() + 12)
+        end.setDate(end.getDate() + 17)
 
-    const app = new Appointment({
-        client: "fulano",
-        start,
-        end,
-    })
+        const string_start = String(start)
+        const string_End = String(end)
 
 
-    
 
 
+        const start_1 = string_start.slice(8,10)
+        const start_2 = Number(start_1)
+        
+        const ends_1 = string_End.slice(8,10)
+        const ends_2 = Number(ends_1)
+
+        let DB = [0]
+
+
+        for (let x = start_2; x < ends_2; x++) {
+            DB.push(x)
+        }
+
+
+        const inicio = 18;
+        const fim = 20
+
+
+        if (DB.includes(inicio) || DB.includes(fim)) {
+            throw new Error("Data ocupada!")  
+        }
+
+        expect(() => {
+            return new Appointment({
+                client: "fulano",
+                start,
+                end
+            })
+        }).toBeTruthy()
+
+   
 })
 
